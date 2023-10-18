cfe419fcd04acc37c2e7d696bf106d78faca763e
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-18 12:40:38 +0400

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,63 @@
+import { MouseEventHandler } from "react";
+
+export type CustomButtonProps = {
+  title: string;
+  containerStyle?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+};
+
+export type SearchManufacturerProps = {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+};
+
+export type CarProps = {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+};
+
+export type FilterProps = {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+};
+
+export type HomeProps = {
+  searchParams: FilterProps;
+};
+
+export type OptionProps = {
+  title: string;
+  value: string;
+};
+
+export type CustomFilterProps = {
+  title: string;
+  options: OptionProps[];
+};
+
+export type ShowMoreProps = {
+  pageNumber: number;
+  isNext: boolean;
+};
+
+export type SearchManuFacturerProps = {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+};
